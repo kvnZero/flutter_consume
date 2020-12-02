1f1b37ab04232839efdb49b53bff2c20198fd1a6
abigeater <abigeater@163.com>
2020-12-03 00:07:58 +0800

    新增记录选择组件

---
+++ lib/ui/widget/common_checkbox.dart
@@ -0,0 +1,63 @@
+import 'dart:developer';
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_consume/common/upx.dart';
+
+class CommonCheckbox extends StatefulWidget {
+  const CommonCheckbox({
+    Key key,
+    @required this.value,
+    @required this.onChanged,
+    this.activeColor,
+    this.checkColor,
+  }) : super(key: key);
+
+  final bool value;
+  final ValueChanged<bool> onChanged;
+  final Color activeColor;
+  final Color checkColor;
+
+
+  @override
+  _CommonCheckboxState createState() => _CommonCheckboxState();
+}
+
+class _CommonCheckboxState extends State<CommonCheckbox> {
+  bool _value = false;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    _value = widget.value;
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () {
+        setState(() {
+          _value = !widget.value;
+        });
+        widget.onChanged(_value);
+      },
+      child: Container(
+        height: upx(10),
+        width: upx(10),
+        decoration: BoxDecoration(shape: BoxShape.circle, color: widget.checkColor ?? Colors.blue),
+        child: _value
+            ? Icon(
+          Icons.check,
+          size: upx(40),
+          color: widget.activeColor ?? Colors.white,
+        )
+            : Icon(
+          Icons.check,
+          size: upx(40),
+          color: Colors.blue,
+        ),
+      ),
+    );
+  }
+}
--- lib/ui/widget/common_record.dart
+++ lib/ui/widget/common_record.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_consume/common/upx.dart';
+import 'package:flutter_consume/common/MoneyEvent.dart';
+import 'package:flutter_consume/ui/widget/common_checkbox.dart';
+import 'package:event_bus/event_bus.dart';
 
 class RecordWidget extends StatefulWidget {
   final int id;
@@ -9,48 +13,86 @@ class RecordWidget extends StatefulWidget {
   final String type;
   final int status;
 
-  RecordWidget({Key key, this.id, this.name, this.money, this.dateShow, this.payDate, this.type, this.status})
-  
+  RecordWidget({Key key, this.id, this.name, this.money, this.dateShow, this.payDate, this.type, this.status});
+
   @override
   _RecordWidgetState createState() => _RecordWidgetState();
 }
 
 class _RecordWidgetState extends State<RecordWidget> {
+
+  bool selected = false;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    if(widget.status == 2){
+      //已勾选
+      selected = true;
+    }
+    super.initState();
+  }
+
+
   @override
   Widget build(BuildContext context) {
-
-//    Image.network("", fit: BoxFit.fill,)'
-
     return Container(
+      margin: EdgeInsets.all(upx(15)),
+      padding: EdgeInsets.all(upx(5)),
+      height: upx(120),
       child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
-          Container(
-              width: 40,
-              height: 40,
-              child: Padding(padding: EdgeInsets.all(0),
-                child:ClipRRect(
-                    borderRadius: BorderRadius.circular(8.0),
-                    child: Icon(Icons.food_bank)
-                ),)
+          Row(
+            children: [
+              Container(
+                  width: upx(50),
+                  height: upx(50),
+                  margin: EdgeInsets.only(right: 10),
+                  child: CommonCheckbox(
+                    value: selected,
+                    activeColor: Colors.white,
+                    checkColor: Colors.blueAccent,
+                    onChanged: (value){
+                      setState(() {
+                        selected = value;
+                      });
+                      eventBus.fire(MoneyChangeInEvent(selected ? widget.money : -widget.money));
+                    },
+                  )
+              ),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Container(
+                      width:upx(500),
+                      child: Text(widget.name, style: TextStyle(fontSize: upx(42),fontWeight: FontWeight.w400, color: Colors.black54, decoration: selected ? TextDecoration.lineThrough : null,),),
+                    ),
+                    Container(
+                      width: upx(500),
+                      child: Text(widget.type, style: TextStyle(fontSize: 12, fontWeight: FontWeight.w300,color: Colors.black26),),
+                    ),
+                  ],
+                ),
+                Container(
+                  width: upx(500),
+                  child: Text("还款日期："+widget.payDate+(selected ? '(已还)' : ''), style: TextStyle(fontSize: 12, fontWeight: FontWeight.w300,color: Colors.black26),),
+                )
+              ],),
+            ],
           ),
-          Column(children: [
-            Container(
-              width: 150,
-              child: Text("Hello", style: TextStyle(fontSize: 12,fontWeight: FontWeight.w300),),
-            ),
-            Container(
-              width: 150,
-              child: Text("电子垃圾", style: TextStyle(fontSize: 12, fontWeight: FontWeight.w300,color: Colors.black26),),
-            ),
-            Container(
-              width: 150,
-              child: Text("12-10", style: TextStyle(fontSize: 12, fontWeight: FontWeight.w300,color: Colors.black26),),
-            )
-          ],),
-          Column(children: [
-            Text(widget.money.toString()),
-            Text(widget.dateShow,)
-          ],),
+          Container(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+              Text(widget.money.toString(), style: TextStyle(fontSize: upx(42), fontWeight: FontWeight.w500),),
+              Text(widget.dateShow, style: TextStyle(fontSize: upx(24), fontWeight: FontWeight.w300, color: Colors.black54), )
+            ],),
+          )
         ],
       ),
     );
