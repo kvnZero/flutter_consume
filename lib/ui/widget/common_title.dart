23521070cdfb7317b19af15b66c5aaa050ff60b7
abigeater <abigeater@163.com>
2020-12-04 23:31:48 +0800

    add 增加右边button

--- lib/ui/widget/common_title.dart
+++ lib/ui/widget/common_title.dart
@@ -6,19 +6,27 @@ class TitleWidget extends StatelessWidget {
   final Color backgroundColor;
   final Color fontColor;
   final double fontSize;
-  TitleWidget({Key key, this.title, this.backgroundColor, this.fontColor, this.fontSize}) : super(key: key);
+  final Widget rightButton;
+
+  TitleWidget({Key key, this.title, this.backgroundColor, this.fontColor, this.fontSize, this.rightButton}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+
+    List<Widget> widgets = [];
+    widgets.add(Text(title,style: TextStyle(fontSize: fontSize, color: fontColor)));
+
+    if(rightButton != null){
+      widgets.add(rightButton);
+    }
+
     return Container(
       color: backgroundColor,
       child: Padding(
         padding: EdgeInsets.only(left: upx(50),right: upx(50),top: MediaQueryData.fromWindow(window).padding.top+10),
         child: Row(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: <Widget>[
-            Text(title,style: TextStyle(fontSize: fontSize, color: fontColor), ),
-          ],
+          children: widgets,
         ),
       ),
     );
