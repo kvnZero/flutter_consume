7345d280f86c6f355b8c7df8ca8b4dfddc5df92d
abigeater <abigeater@163.com>
2020-12-15 22:49:54 +0800

    del TODO

--- lib/ui/page/bill_page.dart
+++ lib/ui/page/bill_page.dart
@@ -50,7 +50,6 @@ class _BillPageState extends State<BillPage> with AutomaticKeepAliveClientMixin{
   }
 
   @override
-  // TODO: implement wantKeepAlive
   bool get wantKeepAlive => true;
 
   @override
