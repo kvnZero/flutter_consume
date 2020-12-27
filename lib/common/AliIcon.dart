fb25729faa15244b91ba1d9fbdf464bca7ca8319
abigeater <abigeater@163.com>
2020-12-27 22:29:09 +0800

    add 新增ALI ICON

---
+++ lib/common/AliIcon.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/cupertino.dart';
+
+class AliIcon {
+
+  static const IconData kafei = IconData(0x343e, fontFamily: 'AliIcon', matchTextDirection: true);
+  static const IconData hanbao = IconData(0x3440, fontFamily: 'AliIcon', matchTextDirection: true);
+  static const IconData kouhong = IconData(0x3441, fontFamily: 'AliIcon', matchTextDirection: true);
+  static const IconData shouji = IconData(0x3448, fontFamily: 'AliIcon', matchTextDirection: true);
+  static const IconData liebiao = IconData(0x344a, fontFamily: 'AliIcon', matchTextDirection: true);
+  static const IconData tianmaotigongfapiao = IconData(0xf007c, fontFamily: 'AliIcon', matchTextDirection: true);
+  static const IconData tianmaohuodaofukuan = IconData(0xf008c, fontFamily: 'AliIcon', matchTextDirection: true);
+  static const IconData xinyongqiafenqi = IconData(0xf0242, fontFamily: 'AliIcon', matchTextDirection: true);
+  static const IconData jiage = IconData(0xf027f, fontFamily: 'AliIcon', matchTextDirection: true);
+  static const IconData huabei = IconData(0xf0289, fontFamily: 'AliIcon', matchTextDirection: true );
+
+}
